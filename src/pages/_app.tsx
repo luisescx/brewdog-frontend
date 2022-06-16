69fdd9ca676d037c23a40e191473f293515a4450
luisescx <luis.esculapio@live.com>
2022-06-15 21:48:28 -0300

    feat: added theme provider in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,27 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "styles/global";
+import theme from "styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>Brewdog</title>
+        <title>BrewDog</title>
 
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
 
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A project to list all the beers from the BrewDog brewery"
         />
       </Head>
 
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
