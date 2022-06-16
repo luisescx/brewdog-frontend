6b1db693eb1444a6e7dedae79f8376521672f051
luisescx <luis.esculapio@live.com>
2022-06-16 18:52:52 -0300

    chore: added SessionProvider from next auth in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,30 @@
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "styles/global";
 import theme from "styles/theme";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>BrewDog</title>
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>BrewDog</title>
 
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
 
-        <meta
-          name="description"
-          content="A project to list all the beers from the BrewDog brewery"
-        />
-      </Head>
+          <meta
+            name="description"
+            content="A project to list all the beers from the BrewDog brewery"
+          />
+        </Head>
 
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
