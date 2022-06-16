309c1b1f340d313d38fb253f4f65917b99a0d2f9
luisescx <luis.esculapio@live.com>
2022-06-15 21:46:40 -0300

    feat: added theme style

---
+++ src/styles/theme.ts
@@ -0,0 +1,39 @@
+export default {
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: "1.2rem",
+      small: "1.4rem",
+      medium: "1.6rem",
+      large: "1.8rem",
+      xlarge: "2.0rem",
+      xxlarge: "2.8rem",
+      huge: "5.2rem"
+    }
+  },
+  colors: {
+    primary: "#F6C101",
+    secondary: "#074c97",
+    mainBg: "#06092B",
+    lightBg: "#F2F2F2",
+    white: "#FAFAFA",
+    black: "#030517",
+    lightGray: "#EAEAEA",
+    gray: "#8F8F8F",
+    darkGray: "#2E2F42",
+    red: "#FF6347"
+  },
+  spacings: {
+    xxsmall: "0.8rem",
+    xsmall: "1.6rem",
+    small: "2.4rem",
+    medium: "3.2rem",
+    large: "4.0rem",
+    xlarge: "4.8rem",
+    xxlarge: "5.6rem"
+  }
+} as const;
