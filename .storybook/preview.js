21f6096a72ef2c2be038c2e810280ddff289b2ab
luisescx <luis.esculapio@live.com>
2022-06-15 21:10:34 -0300

    feat: added styled components and styled-media-query

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../src/styles/global.ts";
+import theme from "../src/styles/theme";
 
 export const decorators = [
   (Story) => (
-    <>
-      <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles removeBg />
 
       <Story />
-    </>
+    </ThemeProvider>
   )
 ];
