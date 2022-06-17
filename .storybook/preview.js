e287c0fc3c826c9cbf302dad786500dae1aa6a46
luisescx <luis.esculapio@live.com>
2022-06-17 12:16:31 -0300

    chore: storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,22 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../src/styles/global.ts";
 import theme from "../src/styles/theme";
 
+export const parameters = {
+  backgrounds: {
+    default: "brewDog-dark",
+    values: [
+      {
+        name: "brewDog-light",
+        value: theme.colors.lightBg
+      },
+      {
+        name: "brewDog-dark",
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+};
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
